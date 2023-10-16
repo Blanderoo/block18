570c18b2561f27b22b11fee552b923a778d744ea
blandero <landerosb08@gmail.com>
2023-10-16 17:46:25 -0600

    Initial commit after clone

---
+++ testingGuided.js
@@ -0,0 +1,28 @@
+// Instructions Block 18 Testing: Elevator - Guided Practice
+//https://fullstack.instructure.com/courses/731/pages/block-18-testing-2?module_item_id=226964
+
+//Functionality of the button panel on their elevators
+//The goal is to replace the outdated panel in over 1,200 different locations. Before you begin coding, you should make a list of all the ways these panels should be tested to make sure they are working correctly and respond to unexpected input in a way that makes sense
+
+// Happy Path 
+//Unhappy path 
+
+
+//Specs
+// pressing elevator button calls elevator to come to your floor (button lights up to show received input)
+//door opens 
+//door sound/elevator music
+//user selects floor they want to go to
+//button lights up
+//doors close 
+//elevator goes to requested floor
+//elevator stops
+//door opens
+//door closes after a short amount of time if no input is received 
+//if multiple floors are selected elevator will move to the called floors in order 
+//elevator stops until next user adds input/presses button
+
+//elevator can check weight capacity and go to the right floor. 
+
+//if emergency button is pressed elevator will stop 
+//doors open
